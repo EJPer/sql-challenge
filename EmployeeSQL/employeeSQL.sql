c69d51d103531b1f30741bb522aee43a5e48e4ae
EJPer <eduardojoseperis@gmail.com>
2023-03-01 19:13:32 -0600

    edits post key rework

--- EmployeeSQL/employeeSQL.sql
+++ EmployeeSQL/employeeSQL.sql
@@ -1,138 +1,14 @@
---Employees Table 
-
---drop statment if needed
-DROP TABLE departments
-
-create table departments (
-	dept_no VARCHAR(60),
-	dept_name VARCHAR(60),
-	PRIMARY KEY (dept_no)
-)
-
-
---SELECT * TO CHECK
-SELECT * FROM DEPARTMENTS
-
-
-
-
-
-
-
-
-
----------------------------------------------------------------------
---empoyee number data
---DROP IF NEEDED
-drop table employee_dept
-
---create empoyee number table
-CREATE TABLE employee_dept (
-	employee_number integer not null,
-	dept_no varchar(30) not null,
-	primary key (employee_number, dept_no),
-	foreign key (dept_no) REFERENCES DEPARTMENTS(dept_no)
-	
-)
-
-
---SELECT * TO CHECK
-SELECT * FROM employee_dept
-
-
-
-
----------------------------------------------------------------------
---dept manger table creation
---drop table if needed
-drop table employeemanage
-
---creating table
-CREATE TABLE employeemanage (
-	dept_no VARCHAR(30) not null,
-	employee_number integer not null,
-	PRIMARY KEY (dept_no, employee_number),
-	foreign key (dept_no) REFERENCES DEPARTMENTS(dept_no)
-)
-
---select * to check
-SELECT * FROM employeemanage
-
-select count(*) from employeemanage
-
-
----------------------------------------------------------------------
---create employee table
-
---drop table if needed
-DROP TABLE employeeinfo
-
-
---employee info create 
-CREATE TABLE employeeinfo (
-	employee_number integer NOT null,
-	emp_title_id VARCHAR(60),
-	birth_date VARCHAR(60),
-	first_name VARCHAR(60) not null,
-	last_name VARCHAR(60) not null,
-	sex VARCHAR(60),
-	hire_date VARCHAR(60),
-	primary key (first_name,last_name,birth_date, employee_number)
-	
-)
-
-
---select * to chec
-SELECT * FROM employeeinfo
-
----------------------------------------------------------------------
-
---drop table if needed
-DROP TABLE salaries
-
---create salaries table
-CREATE TABLE salaries (
-	employee_number integer not null,
-	salary integer not null,
-	primary key (employee_number, salary)
-	
-)
-
---select * to check
-SELECT * FROM salaries
-
-select count(*) from salaries 
-
----------------------------------------------------------------------
-
---drop table if needed
-DROP TABLE titles
-
-
---create titles table
-CREATE TABLE titles (
-	title_id varchar(30),
-	title varchar(30),
-	primary key (title_id)
-)
-
-
---select * to check
-select * from titles
-
----------------------------------------------------------------------
 
 --employee number, last name, first name, sex and salary
-SELECT p1.employee_number, p1.last_name, p1.first_name,p1.sex, p2.salary
-FROM employeeinfo as p1
+SELECT p1.emp_number, p1.last_name, p1.first_name,p1.sex, p2.salary
+FROM "employee_info" as p1
 INNER JOIN salaries as p2 ON
-p1.employee_number= p2.employee_number;
-
+p1."emp_number"= p2."emp_no";
 
 --List the first name, last name, and hire date for the employees who were hired in 1986
-SELECT birth_date, first_name, last_name
-from employeeinfo
-where right(hire_date,4) = '1986'
+SELECT hire_date, first_name, last_name
+from "employee_info"
+where left(cast(hire_date as char(10)),4) = '1986'
 
 
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
@@ -141,11 +17,11 @@ where right(hire_date,4) = '1986'
 -- need first and last name from employeeinfo table
 -- join everything back to manager table
 
-SELECT  p1.dept_no, p3.dept_name, p1.employee_number, p2.first_name, p2.last_name
-FROM employeemanage as p1
-INNER JOIN employeeinfo as p2 ON
-p1.employee_number= p2.employee_number
-INNER JOIN departments as p3 ON
+SELECT  p1.dept_no, p3.dept_name, p1.employee_no, p2.first_name, p2.last_name
+FROM employee_manage as p1
+INNER JOIN employee_info as p2 ON
+p1."employee_no" = p2."emp_number"
+INNER JOIN "department" as p3 ON
 p1.dept_no = p3.dept_no
 ; 
 
@@ -154,41 +30,44 @@ p1.dept_no = p3.dept_no
 --need dept_no from employee depts
 --need dept_names from departments
 -- join empoyee info to employee dept then employee departments to departments
-SELECT p1.employee_number, p1.first_name, p1.last_name, p2.dept_no , p3.dept_name
-from employeeinfo as p1
-INNER JOIN employee_dept as p2 ON
-p1.employee_number = p2.employee_number
-INNER JOIN departments as p3 ON
-p2.dept_no = p3.dept_no
-
+SELECT p1.emp_number, p1.first_name, p1.last_name, p2."department_no" , p3.dept_name
+from employee_info as p1
+INNER JOIN "Employeedept" as p2 ON
+p1.emp_number = p2."employee_number"
+INNER JOIN department as p3 ON
+p2."department_no" = p3.dept_no
+;
 --Find first name Hercules and last names beginning with the letter B
 --no joins needed us employee info table
 SELECT first_name, last_name, sex 
-from employeeinfo
+from employee_info
 where first_name = 'Hercules' and last_name like 'B%'
-
+;
 --list employee #, first name and last name of all employees in the sales department
-SELECT p1.employee_number, p1.first_name, p1.last_name, p2.dept_no , p3.dept_name
-from employeeinfo as p1
-INNER JOIN employee_dept as p2 ON
-p1.employee_number = p2.employee_number
-INNER JOIN departments as p3 ON
-p2.dept_no = p3.dept_no
+SELECT p1.emp_number, p1.first_name, p1.last_name, p2.department_no , p3.dept_name
+from employee_info as p1
+INNER JOIN "Employeedept" as p2 ON
+p1.emp_number = p2.employee_number
+INNER JOIN department as p3 ON
+p2."department_no" = p3.dept_no
 where p3.dept_name = 'Sales'
+;
 
 --list employee #, first name and last name of all employees in the sales and devlopment department
-SELECT p1.employee_number, p1.first_name, p1.last_name, p2.dept_no , p3.dept_name
-from employeeinfo as p1
-INNER JOIN employee_dept as p2 ON
-p1.employee_number = p2.employee_number
-INNER JOIN departments as p3 ON
-p2.dept_no = p3.dept_no
+SELECT p1.emp_number, p1.first_name, p1.last_name, p2.department_no , p3.dept_name
+from employee_info as p1
+INNER JOIN "Employeedept" as p2 ON
+p1.emp_number = p2.employee_number
+INNER JOIN department as p3 ON
+p2."department_no"= p3.dept_no
 where p3.dept_name in ('Sales','Development')
+
 
 --order frequency of employee's last names from ascending to descending order.
 SELECT last_name, COUNT(*)
-FROM employeeinfo
+FROM employee_info
 group by last_name
+order by COUNT(*) DESC
 
 
 
---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,71 @@
+CREATE TABLE "department" (
+    "dept_no" CHAR(10)   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_department" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+
+CREATE TABLE "Employeedept" (
+    "employee_number" integer   NOT NULL,
+    "department_no" CHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Employeedept" PRIMARY KEY (
+        "employee_number","department_no"
+     )
+);
+
+
+
+
+CREATE TABLE "employee_manage" (
+    "dept_no" VARCHAR   NOT NULL,
+	"employee_no" integer   NOT NULL,
+    CONSTRAINT "pk_employee_manage" PRIMARY KEY (
+        "employee_no"
+     )
+);
+
+
+CREATE TABLE "employee_info" (
+    "emp_number" Integer   NOT NULL,
+    "emp_title_id" CHAR(5)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" CHAR   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employee_info" PRIMARY KEY (
+        "emp_number"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+
+
+CREATE TABLE "title" (
+    "title_id" CHAR(5)   NOT NULL,
+    "title" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_title" PRIMARY KEY (
+        "title"
+     )
+);
+
+ALTER TABLE "Employeedept" ADD CONSTRAINT "fk_Employeedept_department_no" FOREIGN KEY("department_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "employee_manage" ADD CONSTRAINT "fk_employee_manage_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "employee_info" ADD CONSTRAINT "fk_employee_info_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "title" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employee_info" ("emp_number");
